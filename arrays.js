e97b6983a18e3c50ec0071a2860c541ead7170ec
Shel Orock <sdo243@nyu.edu>
2018-04-02 19:39:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,14 +1,14 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  const myArray = [1];
-  const myNewArray = ["foo", ...myArray];
+  const myArray = array;
+  const myNewArray = [element, ...array];
   return myNewArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   var myDestructiveArray = array;
-  myDestructiveArray.unshift ("foo");
+  myDestructiveArray.unshift (element);
   return myDestructiveArray;
 }
 
